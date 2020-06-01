4a8f95cc7f9f6a9f70f3297e62ad53fc6847e2a5
Marszht <1046209799@qq.com>
2020-06-01 12:33:03 +0800

    <A> 新增 选择排序

--- sort/selectionSort.js
+++ sort/selectionSort.js
@@ -8,5 +8,25 @@
 
 
   var selectionSort = (arr) => {
-    
+    const sortArr = [...arr];
+    let len = sortArr.length;
+    let temp;
+    console.time('selectionSort');
+    for(let i = 0; i < len; i++) {
+      let minValueIndex = i;
+      for (let j = i + 1; j < len; j++) {
+         if (sortArr[minValueIndex] > sortArr[j]) {
+           minValueIndex = j;
+         }
+      }
+      [sortArr[i], sortArr[minValueIndex]] = [sortArr[minValueIndex],sortArr[i]];
+    }
+    console.timeEnd('selectionSort')
+    return sortArr;
+
+
   }
+
+  let arr = [78,43,35,2,3,66,89,43, 23, 1]
+
+  console.log(selectionSort(arr));
